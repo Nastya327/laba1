38fb1f98a47a174f82a6d792a8eac3703029354f
Nastya <for.better.the.life@gmail.com>
2020-10-22 20:55:53 +0300

    Initial commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьСборкуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(460, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьСборкуToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьСборкуToolStripMenuItem
+            // 
+            this.открытьСборкуToolStripMenuItem.Name = "открытьСборкуToolStripMenuItem";
+            this.открытьСборкуToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.открытьСборкуToolStripMenuItem.Text = "Открыть сборку";
+            this.открытьСборкуToolStripMenuItem.Click += new System.EventHandler(this.открытьСборкуToolStripMenuItem_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(0, 25);
+            this.treeView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(452, 317);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "im1.png");
+            this.imageList1.Images.SetKeyName(1, "im2.jpg");
+            this.imageList1.Images.SetKeyName(2, "im3.jpg");
+            this.imageList1.Images.SetKeyName(3, "im4.jpg");
+            this.imageList1.Images.SetKeyName(4, "im5.jpg");
+            this.imageList1.Images.SetKeyName(5, "im6.jpg");
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(452, 24);
+            this.listView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(8, 318);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(460, 341);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Laba2";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьСборкуToolStripMenuItem;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+       private Assembly assembly;
+        public int[] testArray= new int[] {1,2,3};
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string selectAssemblyFile()
+        {
+            openFileDialog1.Filter = "Dll files (*.dll)|*.dll|Exe files (*.exe)|*.exe|All files (*.*)|*.*";
+            openFileDialog1.Title = "Select assembly file";
+            return (openFileDialog1.ShowDialog() == DialogResult.OK) ? openFileDialog1.FileName : null;
+        }
+        private Assembly openAssembly(string path)
+        {
+            try
+            {
+                Assembly a = Assembly.LoadFrom(path);
+                return a;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Не удалось загрузить указанную сборку!",
+                "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+
+        void addRoot(TreeNode root, Type[] types)
+        {
+            TreeNode node = null;
+            foreach (Type type in types)
+            {
+                node = new TreeNode();
+                node.Text = type.ToString();
+                //Если класс
+                if (type.IsClass)
+                {
+                    node.ImageIndex = 1;
+                    node.SelectedImageIndex = 1;
+                    addFirstLevel(node, type);
+                    root.Nodes.Add(node);
+                }
+                //Если интерфейс
+                else if (type.IsInterface)
+                {
+                    node.ImageIndex = 2;
+                    node.SelectedImageIndex = 2;
+                    addFirstLevel(node, type);
+                    root.Nodes.Add(node);
+                }
+                //Если перечисление
+                else if (type.IsEnum)
+                {
+                    node.ImageIndex = 3;
+                    node.SelectedImageIndex = 3;
+                    addFirstLevel(node, type);
+                    root.Nodes.Add(node);
+                }
+                else if (type.IsValueType && !type.IsPrimitive)
+                {
+                    node.ImageIndex = 4;
+                    node.SelectedImageIndex = 4;
+                    addFirstLevel(node, type);
+                    root.Nodes.Add(node);
+                }
+                //Если массив
+                else if (type.IsArray)
+                {
+                    node.ImageIndex = 7;
+                    node.SelectedImageIndex = 7;
+                    addFirstLevel(node, type);
+                    root.Nodes.Add(node);
+                }
+            }
+        }
+        //Загрузить все поля, конструкторы и методы
+        private void addFirstLevel(TreeNode node, Type type)
+        {
+            TreeNode node1 = null;
+            FieldInfo[] fields = type.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+            MethodInfo[] methods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+            ConstructorInfo[] constructors = type.GetConstructors(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+            //Загрузить поля
+            foreach (FieldInfo field in fields)
+            {
+                node1 = new TreeNode();
+                node1.Text = field.FieldType.Name + " " + field.Name;
+                node1.ImageIndex = 6;
+                node1.SelectedImageIndex = 6;
+                node.Nodes.Add(node1);
+            }
+            //Загрузить конструкторы
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                String s = "";
+                ParameterInfo[] parametrs = constructor.GetParameters();
+                foreach (ParameterInfo parametr in parametrs)
+                {
+                    s = s + parametr.ParameterType.Name + ", ";
+                }
+                s = s.Trim();
+                s = s.TrimEnd(',');
+                node1 = new TreeNode();
+                node1.Text = node.Text + "(" + s + ")";
+                node1.ImageIndex = 5;
+                node1.SelectedImageIndex = 5;
+                node.Nodes.Add(node1);
+            }
+            //Загрузить методы
+            foreach (MethodInfo method in methods)
+            {
+                String s = "";
+                ParameterInfo[] parametrs = method.GetParameters();
+                foreach (ParameterInfo parametr in parametrs)
+                {
+                    s = s + parametr.ParameterType.Name + ", ";
+                }
+                s = s.Trim();
+                s = s.TrimEnd(',');
+                node1 = new TreeNode();
+                node1.Text = method.ReturnType.Name + " " + method.Name + "("
+                + s + ")";
+                node1.ImageIndex = 4;
+                node1.SelectedImageIndex = 4;
+                node.Nodes.Add(node1);
+            }
+            foreach (FieldInfo array in fields)
+            {
+                node1 = new TreeNode();
+                node1.Text = array.FieldType.Name + " " + array.Name;
+                node1.ImageIndex = 7;
+                node1.SelectedImageIndex = 7;
+                node.Nodes.Add(node1);
+            }
+        }
+
+        private void открытьСборкуToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+            string path = selectAssemblyFile();
+            if (path != null)
+            {
+                assembly = openAssembly(path);
+            }
+            if (assembly != null)
+            {
+                TreeNode root = new TreeNode();
+                root.Text = assembly.GetName().Name;
+                root.ImageIndex = 0;
+                root.SelectedImageIndex = 0;
+                treeView1.Nodes.Add(root);
+                Type[] types = assembly.GetTypes();
+                addRoot(root, types);
+            }
+        }
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
